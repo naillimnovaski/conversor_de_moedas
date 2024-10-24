30dc1f1f0b8173311f9b5a38097381b9a6033b56
naillim <naillim.domingues@alunoescolassociais.org.br>
2024-10-24 07:30:14 -0300

    Pega os valores inseridos pelo usuário

---
+++ conversor.js
@@ -0,0 +1,7 @@
+ 
+document.getElementById('currency-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+ // Pega os valores inseridos pelo usuário
+    const amount = parseFloat(document.getElementById('amount').value);
+    const fromCurrency = document.getElementById('from-currency').value;
+    const toCurrency = document.getElementById('to-currency').value;
